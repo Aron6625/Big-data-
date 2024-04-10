96c1552c54bff26237f9f81c3dc01ca2ba9f2ce0
Aron6625 <aronpad123@gmail.com>
2024-04-10 09:39:02 -0700

    final

--- index.js
+++ index.js
@@ -23,47 +23,67 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const body = req.body;
 
-  const filterZillow = {
-    searchQueryState: {
-      pagination: {
-        currentPage: 1
-      },
-      mapBounds: boundings[body.estado],
-      filterState: tipoVenta[body.operacion](body.minPrice, body.maxPrice),
-      isListVisible: true
-    },
-    wants: {
-      cat1: [ 'listResults' ],
-      cat2: [ 'total' ]
-    },
-    requestId: 2,
-    isDebugRequest: false
-  }
-
   try {
-    const dacker = ['Direccion', 'Precio', 'Tipo_Adquicision', 'Habitaciones', 'Banios', 'Ciudad', 'Fecha']
-    fs.writeFileSync('./public/nose.csv', dacker.join(';'));
+    const headers = ['Direccion', 'Precio', 'Tipo_Adquicision', 'Habitaciones', 'Banios', 'Ciudad', 'Fecha']
+    fs.writeFileSync('./public/results.csv', 'sep=;\n' + headers.join(';'));
+    
+    const operaciones = body.operacion.split(',');
 
-    const rest = new RestApi('https://www.zillow.com');
-
-    for(let i=1; i <= 15; i++) {
-      filterZillow.searchQueryState.pagination.currentPage = i;
-
-      const response = await rest.put('/async-create-search-page-state', filterZillow);
-      const results = response.cat1.searchResults.listResults;
-          
-      for(const result of results) {
-        saveData(result, 'nose.csv');
+    for(const operacion of operaciones) {
+      const filterZillow = {
+        searchQueryState: {
+          pagination: {
+            currentPage: 1
+          },
+          mapBounds: boundings[body.estado],
+          filterState: tipoVenta[operacion](body.minPrice, body.maxPrice),
+          isListVisible: true
+        },
+        wants: {
+          cat1: [ 'listResults' ],
+          cat2: [ 'total' ]
+        },
+        requestId: 2,
+        isDebugRequest: false
       }
+
+      await hacerLlamadas(filterZillow);
     }
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', `attachment; filename=zillow_datos.csv`);
+
+    fs.readFile('./public/results.csv', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error downloading file');
+      }
+
+      res.send(data);
+    });
 
     console.log('User updated successfully: ');
   } catch(error) {
     console.log(error);
-  }
 
-  res.json({message: 'OK recopilacion.'})
+    res.json({error: 'OK recopilacion.'})
+  }
 });
+
+async function hacerLlamadas(filterZillow) {
+  const rest = new RestApi('https://www.zillow.com');
+
+  for(let i=1; i <= 10; i++) {
+    filterZillow.searchQueryState.pagination.currentPage = i;
+
+    const response = await rest.put('/async-create-search-page-state', filterZillow);
+    const results = response.cat1.searchResults.listResults;
+        
+    for(const result of results) {
+      saveData(result, 'results.csv');
+    }
+  }
+}
 
 function saveData(data, file) {
   const saveData = {
@@ -77,7 +97,6 @@ function saveData(data, file) {
   };
 
   fs.appendFileSync('./public/' + file, '\n' + Object.values(saveData).join(';'));
-
 }
 
 app.use('/recopilador', router);
